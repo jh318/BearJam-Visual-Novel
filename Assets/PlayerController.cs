7303879baa473a4ea88d83dc7a34d8d64814b437
John Hawley <jhawley318@gmail.com>
2017-12-15 21:06:37 -0600

    set up basic project stuff. made playercontroller. move left and right

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 2.0f;
+
+
+	Rigidbody2D body;
+
+	void Start(){
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	void Update(){
+		GetPlayerInput();
+	}
+
+	void GetPlayerInput(){
+		if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f){
+			body.velocity = new Vector2(Input.GetAxisRaw("Horizontal"),0) * speed ;
+		}
+		else if(Input.GetAxisRaw("Vertical") > 0.5f){
+			//body.AddForce(new Vector2(0,1) * 1000);
+		}
+	}
+}
