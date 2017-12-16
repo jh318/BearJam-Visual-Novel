90611c7edffa38b706e59eed327827c0e463b7a6
John Hawley <jhawley318@gmail.com>
2017-12-16 02:28:46 -0600

    implemented player bear animations, anim tree, transitions, correctly stop player movement and anim when hitting text box.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,22 +8,64 @@ public class PlayerController : MonoBehaviour {
 	public bool canMove;
 
 	Rigidbody2D body;
+	Animator anim;
+	SpriteRenderer spriteRend;
 
 	void Start(){
 		body = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		spriteRend = GetComponentInChildren<SpriteRenderer>();
+		spriteRend.flipX = true;
 	}
 
 	void Update(){
-		if(!canMove) return;
+		if(!canMove){
+			StopPlayerMovement();
+			return;
+		} 
 		GetPlayerInput();
 	}
 
 	void GetPlayerInput(){
-		if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f){
-			body.velocity = new Vector2(Input.GetAxisRaw("Horizontal"),0) * speed ;
+		
+		float directionX = Input.GetAxisRaw("Horizontal");
+		float directionY = Input.GetAxisRaw("Vertical");
+
+		if(Mathf.Abs(directionX) > 0.5f){
+			anim.SetFloat("horizontal", Input.GetAxisRaw("Horizontal"));
+			anim.SetFloat("vertical", 0.0f);
+			anim.SetBool("isMoving", true);
+			if(directionX < -0.5f) 
+				spriteRend.flipX = true;
+			else 
+				spriteRend.flipX = false;
+			body.velocity = new Vector2(directionX, 0) * speed ;
 		}
-		else if(Input.GetAxisRaw("Vertical") > 0.5f){
-			//body.AddForce(new Vector2(0,1) * 1000);
+		else if(Mathf.Abs(directionY) > 0.5f){
+			anim.SetFloat("vertical", Input.GetAxisRaw("Vertical"));
+			anim.SetFloat("horizontal", 0.0f);
+			anim.SetBool("isMoving", true);
+			body.velocity = new Vector2(0, directionY) * speed;
 		}
+		else{
+			anim.SetBool("isMoving", false);
+			body.velocity = Vector2.zero;
+		}
+
+		anim.SetFloat("lastHorizontal", anim.GetFloat("horizontal"));
+		anim.SetFloat("lastVertical", anim.GetFloat("vertical"));
+	}
+
+	void StopPlayerMovement(){
+		body.velocity = Vector3.zero;
+		anim.SetFloat("vertical", 0.0f);
+		anim.SetFloat("horizontal", 0.0f);
+		anim.SetBool("isMoving", false);
+		// anim.SetFloat("lastVertical", 0.0f);
+		// anim.SetFloat("lastHorizontal", 0.0f);
+		if(anim.GetFloat("lastHorizontal") > 0.5f)
+			anim.Play("Idle_Right");
+		else
+			anim.Play("Idle_Left");
 	}
 }
